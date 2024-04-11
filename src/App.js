85ba7cf3b82f5d4a6f52ea2b523a740a5fc357fe
ohadbenisti <ohadbenisti@gmail.com>
2024-04-11 11:44:26 +0300

    useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [productsArr, setProductArr] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProductArr(data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
